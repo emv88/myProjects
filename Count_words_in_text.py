3033cac4b342c6a53500c32dd3990ff213db269a
emv88 <91303738+emv88@users.noreply.github.com>
2022-08-15 23:59:02 -0600

    Update Count_words_in_text.py

--- Count_words_in_text.py
+++ Count_words_in_text.py
@@ -1,7 +1,11 @@
 from collections import Counter
 
 # Text taken from Python para Todos from Charles Severance
-text = "Escribir programas (o programar) es una actividad muy creativa y gratificante. Puedes escribir programas por muchas razones, que pueden ir desde mantenerte activo resolviendo un problema de análisis de datos complejo hasta hacerlo por pura diversión ayudando a otros a resolver un enigma. Este libro asume que todo el mundo necesita saber programar, y que una vez que aprendas a programar ya encontrarás qué quieres hacer con esas habilidades recién adquiridas"
+text =  "Escribir programas (o programar) es una actividad muy creativa y gratificante."\
+        "Puedes escribir programas por muchas razones, que pueden ir desde mantenerte activo resolviendo un problema de "\
+        "análisis de datos complejo hasta hacerlo por pura diversión ayudando a otros a resolver un enigma."\
+        "Este libro asume que todo el mundo necesita saber programar, y que una vez que aprendas a programar ya encontrarás "\
+        "qué quieres hacer con esas habilidades recién adquiridas"
 
 # Convert text to list
 split_it = text.split()
